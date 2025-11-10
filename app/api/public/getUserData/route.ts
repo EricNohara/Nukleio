46101889a604a06b08b97efb7355abad0daede6b
EricNohara <ernohara@bu.edu>
2025-11-10 15:04:25 -0500

    Add debug

--- app/api/public/getUserData/route.ts
+++ app/api/public/getUserData/route.ts
@@ -19,6 +19,9 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
 
     // get the api key from the authorization header
     apiKey = req.headers.get("Authorization")?.split(" ")[1];
+
+    console.log(apiKey);
+
     if (!apiKey) {
       statusCode = 401;
       return NextResponse.json(
@@ -42,6 +45,8 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
     }
 
     keyDescription = data[0].description;
+
+    console.log(keyDescription);
 
     if (keyDescription === "Nukleio Super Key") {
       // if nukleio super key get user id from header
